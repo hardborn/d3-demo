b4c08e7f48aa46461dbacdc09ad91a7127bdc985
Hardborn <hardborn@live.cn>
2017-07-05 17:37:49 +0800

    first commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'd3-demo'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
